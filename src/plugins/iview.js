cd63b860a200f1d693dda54870654dc8bb9325c1
@Aiiua <347565645@qq.com>
2020-03-15 18:09:01 +0800

    feat(plugins): add iview

--- src/plugins/iview.js
+++ src/plugins/iview.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
-import { Button } from 'view-design'
 
-import 'view-design/dist/styles/iview.css'
+import { ButtonGroup, Button, Input, Icon, Row, Col, Message } from 'view-design'
 
+Vue.component('ButtonGroup', ButtonGroup)
 Vue.component('Button', Button)
+Vue.component('Input', Input)
+Vue.component('Icon', Icon)
+Vue.component('Row', Row)
+Vue.component('Col', Col)
+
+Vue.prototype.$Message = Message
